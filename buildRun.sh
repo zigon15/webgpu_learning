df2ce27d6e0b7bb95ad1874234dac0bbc3d8c07b
Simon <ss13andbb12@gmail.com>
2025-11-25 12:52:59 +1030

    src: Initial project setup

---
+++ buildRun.sh
@@ -0,0 +1,5 @@
+cmake . -B build
+cmake --build build
+
+cd build
+./App   
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <webgpu/webgpu.h>
+#include <iostream>
+
+int main (int, char**) {
+  WGPUInstanceDescriptor desc = {};
+  desc.nextInChain = nullptr;
+
+  // We create the instance using this descriptor
+  WGPUInstance instance = wgpuCreateInstance(&desc);
+
+  // We can check whether there is actually an instance created
+  if (!instance) {
+    std::cerr << "Could not initialize WebGPU!" << std::endl;
+    return 1;
+  }
+
+  // Display the object (WGPUInstance is a simple pointer, it may be
+  // copied around without worrying about its size).
+  std::cout << "WGPU instance: " << instance << std::endl;
+
+  return 0;
+}
